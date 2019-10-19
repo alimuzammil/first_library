c7dc9a75974b23bb1fb80ed5bdff9f405a7a3cb3
Bravo <muzammilaliaree@gmail.com>
2019-10-19 21:25:14 +0500

    table one commite

---
+++ first_lib/src/lib.rs
@@ -0,0 +1,12 @@
+pub table_function(mut add_number:u64,mut count_1:u64) {
+
+loop { 
+    println!(" 2 * {} = {}",count_1, add_number);
+    count_1 = count_1 +1 ;
+    add_number = add_number +2;
+    if count_1 ==11  {
+        break
+    }
+
+} 
+}
