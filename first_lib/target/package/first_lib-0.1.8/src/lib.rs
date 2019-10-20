de4f7ccbf49014a0af9c79bf71954be7ffc31796
Bravo <muzammilaliaree@gmail.com>
2019-10-21 00:20:28 +0500

    commit

--- first_lib/target/package/first_lib-0.1.8/src/lib.rs
+++ first_lib/target/package/first_lib-0.1.8/src/lib.rs
@@ -3,7 +3,7 @@ pub fn table_function(mut table:u64,mut count_1:u64) {
 loop { 
     println!(" {} * {} = {}",table , count_1, add_number);
     count_1 = count_1 +1 ;
-    let add_number = table+table;
+    add_number = table+table;
     if count_1 ==11  {
         break
     }
---
+++ first_lib/target/package/first_lib-0.1.8/target/debug/deps/first_lib-1ffb5ccba6b87d63.d
@@ -0,0 +1,7 @@
+/home/bravo/IOT/library_projects/first_library/first_lib/target/package/first_lib-0.1.8/target/debug/deps/first_lib-1ffb5ccba6b87d63.rmeta: src/lib.rs
+
+/home/bravo/IOT/library_projects/first_library/first_lib/target/package/first_lib-0.1.8/target/debug/deps/libfirst_lib-1ffb5ccba6b87d63.rlib: src/lib.rs
+
+/home/bravo/IOT/library_projects/first_library/first_lib/target/package/first_lib-0.1.8/target/debug/deps/first_lib-1ffb5ccba6b87d63.d: src/lib.rs
+
+src/lib.rs:
